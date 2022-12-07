00445b11b918386de1a78f8efa8751b9fe3fb9a6
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2022-12-07 17:24:11 +0530

    PgDetailsdao

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/dao/PGDetailsDao.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/dao/PGDetailsDao.java
@@ -1,5 +1,36 @@
 package com.ty.pgboot_app.pg_app.dao;
 
-public class PGDetailsDao {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.pgboot_app.pg_app.dto.PGDetails;
+import com.ty.pgboot_app.pg_app.repo.PGDetailsRepository;
+
+@Repository
+public class PGDetailsDao {
+	@Autowired
+	private PGDetailsRepository repository;
+	
+	public PGDetails savePGDetails(PGDetails pgDetails ) {
+		return repository.save(pgDetails);
+	}
+	
+	public PGDetails updatePGDetails(PGDetails pgDetails ) {
+		return repository.save(pgDetails);
+	}
+	
+	public PGDetails getPGDetailsById(int id) {
+		Optional<PGDetails> optional = repository.findById(id);
+		if(optional.isEmpty()) {
+			return null;
+		}
+		return optional.get();
+	}
+	
+	public String deletePGDetailsById(int id) {
+		repository.deleteById(id);
+		return "Deleted";
+	}
 }
