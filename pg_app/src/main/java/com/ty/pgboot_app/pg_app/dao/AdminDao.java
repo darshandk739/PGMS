19d89b0f6f0d80d07ac9a4dafaebf772aae1941e
mansoorjavadeveloper <mansoorjavadeveloper@gmail.com>
2022-12-07 17:23:26 +0530

    AdminDao and UserDao

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/dao/AdminDao.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/dao/AdminDao.java
@@ -1,5 +1,36 @@
 package com.ty.pgboot_app.pg_app.dao;
 
-public class AdminDao {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.pgboot_app.pg_app.dto.Admin;
+import com.ty.pgboot_app.pg_app.repo.AdminRepository;
+
+@Repository
+public class AdminDao {
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	public Admin saveAdmin(Admin admin) {
+		return adminRepository.save(admin);
+	}
+	
+	public Admin updateAdmin(Admin admin) {
+		return adminRepository.save(admin);
+	}
+	
+	public Admin getAdminById(int id) {
+		Optional<Admin> optional = adminRepository.findById(id);
+		if(optional.isEmpty()) {
+			return null;
+		}
+		return optional.get();
+	}
+	
+	public String deleteAdminById(int id) {
+		adminRepository.deleteById(id);
+		return "Deleted";
+	}
 }
--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/dao/UserDao.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/dao/UserDao.java
@@ -1,5 +1,35 @@
 package com.ty.pgboot_app.pg_app.dao;
 
-public class UserDao {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.pgboot_app.pg_app.dto.User;
+import com.ty.pgboot_app.pg_app.repo.UserRepository;
+
+@Repository
+public class UserDao {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User saveUser(User user){
+		return userRepository.save(user);
+	}
+	
+	public User updateUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	public User getUserById(int id) {
+		Optional<User> optional = userRepository.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+	public String deleteUserById(int id) {
+		userRepository.deleteById(id);
+		return "Deleted";
+	}
 }
