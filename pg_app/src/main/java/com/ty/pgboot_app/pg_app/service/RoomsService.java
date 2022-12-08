42ac09153f6562fc63e749668c5cde03a483140f
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2022-12-08 16:49:33 +0530

    Update RoomsService.java

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/service/RoomsService.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/service/RoomsService.java
@@ -1,53 +1,48 @@
 package com.ty.pgboot_app.pg_app.service;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.ty.pgboot_app.pg_app.dao.RoomsDao;
 import com.ty.pgboot_app.pg_app.dto.Rooms;
 import com.ty.pgboot_app.pg_app.exception.NoSuchIdFoundException;
 import com.ty.pgboot_app.pg_app.exception.UnableToDeleteException;
+import com.ty.pgboot_app.pg_app.exception.UnableToUpdateException;
 import com.ty.pgboot_app.pg_app.util.ResponseStructure;
 
 @Service
 public class RoomsService {
 	@Autowired
-	
 	private RoomsDao roomsDao;
-	
-	public ResponseEntity<ResponseStructure<Rooms>> saveRooms(Rooms rooms){
-		
+
+	public ResponseEntity<ResponseStructure<Rooms>> saveRooms(Rooms rooms) {
+
 		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
-		
-		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(responseStructure,HttpStatus.CREATED);
+		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
+				responseStructure, HttpStatus.CREATED);
 		responseStructure.setStatus(HttpStatus.CREATED.value());
 		responseStructure.setMessage("Created");
 		responseStructure.setData(roomsDao.saveRooms(rooms));
 		return responseEntity;
 	}
-	
-	public ResponseEntity<ResponseStructure<Rooms>> updateRooms(Rooms rooms,int id){
-		
+
+	public ResponseEntity<ResponseStructure<Rooms>> updateRooms(Rooms rooms, int id) {
+
 		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
-		
-		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(responseStructure,HttpStatus.OK);
+		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
+				responseStructure, HttpStatus.OK);
 		Optional<Rooms> rooms2 = roomsDao.getRoomsById(id);
-		if(rooms2 != null) {
+		if (rooms2 != null) {
 			responseStructure.setStatus(HttpStatus.OK.value());
-			responseStructure.setMessage("Updated");
+			responseStructure.setMessage("Rooms Updated");
 			responseStructure.setData(roomsDao.updateRooms(rooms));
-		}else {
-			throw new NoSuchIdFoundException("no such id found in database") ;
+			return responseEntity;
 		}
-		
-		return responseEntity;
-		
-		
+		throw new UnableToUpdateException("no such id found in database to update");
 	}
+
 	public ResponseEntity<ResponseStructure<Rooms>> getRoomsById(int id) {
 		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
 		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
@@ -76,7 +71,4 @@ public class RoomsService {
 		}
 		throw new UnableToDeleteException("no such id found in database to delete");
 	}
-	
-	
-
 }
