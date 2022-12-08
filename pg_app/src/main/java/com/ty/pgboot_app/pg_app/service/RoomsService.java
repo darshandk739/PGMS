a3e17fef1e0fa4044852759cad13be5c333093c6
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2022-12-08 15:57:55 +0530

    room

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/service/RoomsService.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/service/RoomsService.java
@@ -1,75 +1,54 @@
 package com.ty.pgboot_app.pg_app.service;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import com.ty.pgboot_app.pg_app.dao.RoomsDao;
 import com.ty.pgboot_app.pg_app.dto.Rooms;
 import com.ty.pgboot_app.pg_app.exception.NoSuchIdFoundException;
-import com.ty.pgboot_app.pg_app.exception.UnableToDeleteException;
-import com.ty.pgboot_app.pg_app.exception.UnableToUpdateException;
 import com.ty.pgboot_app.pg_app.util.ResponseStructure;
 
 @Service
 public class RoomsService {
 	@Autowired
+	
 	private RoomsDao roomsDao;
-
-	public ResponseEntity<ResponseStructure<Rooms>> saveRooms(Rooms rooms) {
-
+	
+	public ResponseEntity<ResponseStructure<Rooms>> saveRooms(Rooms rooms){
+		
 		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
-		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
-				responseStructure, HttpStatus.CREATED);
+		
+		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(responseStructure,HttpStatus.CREATED);
 		responseStructure.setStatus(HttpStatus.CREATED.value());
 		responseStructure.setMessage("Created");
 		responseStructure.setData(roomsDao.saveRooms(rooms));
 		return responseEntity;
 	}
-
-	public ResponseEntity<ResponseStructure<Rooms>> updateRooms(Rooms rooms, int id) {
-
+	
+	public ResponseEntity<ResponseStructure<Rooms>> updateRooms(Rooms rooms,int id){
+		
 		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
-		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
-				responseStructure, HttpStatus.OK);
+		
+		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(responseStructure,HttpStatus.OK);
 		Optional<Rooms> rooms2 = roomsDao.getRoomsById(id);
-		if (rooms2 != null) {
+		if(rooms2!=null) {
 			rooms.setRoomsId(id);
 			responseStructure.setStatus(HttpStatus.OK.value());
-			responseStructure.setMessage("Rooms Updated");
+			responseStructure.setMessage("Updated");
 			responseStructure.setData(roomsDao.updateRooms(rooms));
-			return responseEntity;
+		}else {
+			throw new NoSuchIdFoundException("no such id found in database") ;
 		}
-		throw new UnableToUpdateException("no such id found in database to update");
+		
+		return responseEntity;
+		
+		
 	}
+	
+	
 
-	public ResponseEntity<ResponseStructure<Rooms>> getRoomsById(int id) {
-		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
-		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
-				responseStructure, HttpStatus.OK);
-		Optional<Rooms> rooms2 = roomsDao.getRoomsById(id);
-		if (rooms2 != null) {
-			responseStructure.setStatus(HttpStatus.OK.value());
-			responseStructure.setMessage("Get Rooms");
-			responseStructure.setData(roomsDao.getRoomsById(id).get());
-			return responseEntity;
-		}
-		throw new NoSuchIdFoundException("no such id found in database");
-	}
-
-	public ResponseEntity<ResponseStructure<Rooms>> deleteRoomsById(int id) {
-		ResponseStructure<Rooms> responseStructure = new ResponseStructure<Rooms>();
-		ResponseEntity<ResponseStructure<Rooms>> responseEntity = new ResponseEntity<ResponseStructure<Rooms>>(
-				responseStructure, HttpStatus.OK);
-		Optional<Rooms> optional = roomsDao.getRoomsById(id);
-		if (optional != null) {
-			roomsDao.deleteRooms(optional.get());
-			responseStructure.setStatus(HttpStatus.OK.value());
-			responseStructure.setMessage("Rooms Deleted");
-			responseStructure.setData(optional.get());
-			return responseEntity;
-		}
-		throw new UnableToDeleteException("no such id found in database to delete");
-	}
 }
