4b1827aabd4b10520e97e6cced8047c668ceaeba
mansoorjavadeveloper <mansoorjavadeveloper@gmail.com>
2022-12-08 17:26:20 +0530

    Admin and User Controller Added

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/controller/AdminController.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/controller/AdminController.java
@@ -1,5 +1,64 @@
 package com.ty.pgboot_app.pg_app.controller;
 
-public class AdminController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ty.pgboot_app.pg_app.dto.Admin;
+import com.ty.pgboot_app.pg_app.service.AdminService;
+import com.ty.pgboot_app.pg_app.util.ResponseStructure;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+public class AdminController {
+	@Autowired
+	private AdminService adminService;
+	
+	@ApiOperation(value = "SaveAdmin", notes = "It is used to Save the Admin")
+	@ApiResponses(value = {@ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+	produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<Admin>> saveAdmin(@RequestBody Admin admin){
+		return adminService.saveAdmin(admin);
+	}
+	
+	@ApiOperation(value = "UpdateAdmin", notes = "It is used to Update the Admin")
+	@ApiResponses(value = {@ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<Admin>> updateAdmin(@RequestBody Admin admin, @RequestParam int id){
+		return adminService.updateAdmin(admin, id);
+	}
+	
+	@ApiOperation(value = "GetAdmin", notes = "It is used to Get the Admin")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<Admin>> getAdminById(@RequestParam int id){
+		return adminService.getAdminById(id);
+	}
+	
+	@ApiOperation(value = "DeleteAdmin", notes = "It is used to Delete the Admin")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@DeleteMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<Admin>> deleteAdmin(@RequestParam int id){
+		return adminService.deleteAdmin(id);
+	}
 }
--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/controller/UserController.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/controller/UserController.java
@@ -1,5 +1,64 @@
 package com.ty.pgboot_app.pg_app.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ty.pgboot_app.pg_app.dto.User;
+import com.ty.pgboot_app.pg_app.service.UserService;
+import com.ty.pgboot_app.pg_app.util.ResponseStructure;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@ApiOperation(value = "SaveUser", notes = "It is used to Save the User")
+	@ApiResponses(value = {@ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+	produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<User>> saveUser(@RequestBody User user){
+		return userService.saveUser(user);
+	}
+	
+	@ApiOperation(value = "UpdateUser", notes = "It is used to Update the User")
+	@ApiResponses(value = {@ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<User>> updateUser(@RequestBody User user, @RequestParam int id){
+		return userService.updateUser(user, id);
+	}
+	
+	@ApiOperation(value = "GetUser", notes = "It is used to Get the User")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<User>> getUserById(@RequestParam int id){
+		return userService.getUserById(id);
+	}
+	
+	@ApiOperation(value = "DeleteUser", notes = "It is used to Delete the User")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Internal Server Error"),
+			@ApiResponse(code = 404, message = "Not Found")})
+	@DeleteMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ResponseStructure<User>> deleteUser(int id){
+		return userService.deleteUser(id);
+	}
 }
