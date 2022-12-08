a7705537dd9707bf383b2d0b691906f7e7105635
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2022-12-08 13:03:15 +0530

    PgService

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/service/PGDetailsService.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/service/PGDetailsService.java
@@ -1,5 +1,7 @@
 package com.ty.pgboot_app.pg_app.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,9 @@ import org.springframework.stereotype.Service;
 
 import com.ty.pgboot_app.pg_app.dao.PGDetailsDao;
 import com.ty.pgboot_app.pg_app.dto.PGDetails;
+import com.ty.pgboot_app.pg_app.exception.NoSuchIdFoundException;
+import com.ty.pgboot_app.pg_app.exception.UnableToDeleteException;
+import com.ty.pgboot_app.pg_app.exception.UnableToUpdateException;
 import com.ty.pgboot_app.pg_app.util.ResponseStructure;
 
 @Service
@@ -14,15 +19,61 @@ public class PGDetailsService {
 
 	@Autowired
 	PGDetailsDao dao;
-	
+
 	public ResponseEntity<ResponseStructure<PGDetails>> savePGDetails(PGDetails pgDetails) {
 		ResponseStructure<PGDetails> responseStructure = new ResponseStructure<PGDetails>();
-		ResponseEntity<ResponseStructure<PGDetails>> responseEntity =new ResponseEntity<ResponseStructure<PGDetails>>(responseStructure,HttpStatus.CREATED);
+		ResponseEntity<ResponseStructure<PGDetails>> responseEntity = new ResponseEntity<ResponseStructure<PGDetails>>(
+				responseStructure, HttpStatus.OK);
 		responseStructure.setStatus(HttpStatus.CREATED.value());
 		responseStructure.setMessage("Saved");
 		responseStructure.setData(dao.savePGDetails(pgDetails));
 		return responseEntity;
 	}
-	
-	
+
+	public ResponseEntity<ResponseStructure<PGDetails>> updatePGDetails(PGDetails pgDetails, int id) {
+		Optional<PGDetails> optional = dao.getPGDetailsById(id);
+		ResponseStructure<PGDetails> responseStructure = new ResponseStructure<PGDetails>();
+		ResponseEntity<ResponseStructure<PGDetails>> responseEntity = new ResponseEntity<ResponseStructure<PGDetails>>(
+				responseStructure, HttpStatus.OK);
+		if (optional.isPresent()) {
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setMessage("Updated");
+			responseStructure.setData(dao.updatePGDetails(pgDetails));
+
+			return responseEntity;
+		}
+		throw new UnableToUpdateException();
+	}
+
+	public ResponseEntity<ResponseStructure<PGDetails>> getPGDetails(PGDetails pgDetails, int id) {
+		Optional<PGDetails> optional = dao.getPGDetailsById(id);
+		ResponseStructure<PGDetails> responseStructure = new ResponseStructure<PGDetails>();
+		ResponseEntity<ResponseStructure<PGDetails>> responseEntity = new ResponseEntity<ResponseStructure<PGDetails>>(
+				responseStructure, HttpStatus.OK);
+		if (optional.isPresent()) {
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setMessage("Retrieved");
+			responseStructure.setData(dao.getPGDetailsById(id).get());
+
+			return responseEntity;
+		}
+		throw new NoSuchIdFoundException();
+	}
+
+	public ResponseEntity<ResponseStructure<PGDetails>> deletePGDetails(int id) {
+		Optional<PGDetails> optional = dao.getPGDetailsById(id);
+		ResponseStructure<PGDetails> responseStructure = new ResponseStructure<PGDetails>();
+		ResponseEntity<ResponseStructure<PGDetails>> responseEntity = new ResponseEntity<ResponseStructure<PGDetails>>(
+				responseStructure, HttpStatus.OK);
+		if (optional.isPresent()) {
+			dao.deletePGDetails(optional.get());
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setMessage("Deleted");
+			responseStructure.setData(dao.savePGDetails(optional.get()));
+
+			return responseEntity;
+		}
+		throw new UnableToDeleteException();
+	}
+
 }
