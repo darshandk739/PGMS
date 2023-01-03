7c486c7456f97905957b2c8b972ab6a198cf4d3e
lakshminarayanjavadeveloper <lakshminarayanjavadeveloper@gmail.com>
2023-01-03 12:00:27 +0530

    changes in RoomsController

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/dto/Admin.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/dto/Admin.java
@@ -9,7 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
