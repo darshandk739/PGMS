d021385fbd48a16c1df9d02564e37e12be1bf402
mansoorjavadeveloper <mansoorjavadeveloper@gmail.com>
2023-01-04 12:01:36 +0530

    Admin delete api and field validation

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/dto/Admin.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/dto/Admin.java
@@ -7,13 +7,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -24,23 +24,20 @@ import lombok.Setter;
 public class Admin {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
-	@NotBlank(message = "Please fill the reqiured")
-
-	@NotBlank
-
 	private int adminId;
+	@NotNull
 	private String adminName;
-	@NotNull
+	@NotBlank(message = "User name can not be blank, Please fill the valid Name")
 	private String adminUserName;
-	@NotNull
+	@Email(message = "Please enter the valid email address")
 	private String adminEmail;
-	@NotNull
+	@NotBlank(message = "Please enter the Password")
 	private String adminPassword;
 	@OneToMany
+	@JsonIgnore
 	private List<PGDetails> pgDetails;
 	@OneToMany
+	@JsonIgnore
 	private List<User> users;
 	
 }
