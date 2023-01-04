424d36cf978ce8caea8cb2aa7e298f6059e8b9d2
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2023-01-04 14:48:44 +0530

    change

--- pg_app/src/main/java/com/ty/pgboot_app/pg_app/dto/PGDetails.java
+++ pg_app/src/main/java/com/ty/pgboot_app/pg_app/dto/PGDetails.java
@@ -8,6 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -20,17 +23,26 @@ public class PGDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int pgId;
+	@NotBlank(message="Please enter a valid PG name")
 	private String pgName;
+	@NotBlank(message="Please enter a valid PG owner name")
 	private String pgOwnername;
+	@NotBlank(message="Please enter a valid PG location")
 	private String pgLocation;
+	@NotBlank(message="Please enter a valid PG Phone number")
 	private long pgPhone;
+	@NotBlank(message="Please enter a valid status")
 	private String status;
+	@NotBlank(message="Please enter valid PG facilities")
 	private String pgFacilities;
-	private String pgFoods;
+	@NotBlank(message="Please enter valid PG Foods")
+	private String pgFoods;   
 
 
+	
+	
 	@OneToMany(cascade = CascadeType.ALL)
-
+    @JsonIgnore
 	private List<Rooms> rooms;
 
 
